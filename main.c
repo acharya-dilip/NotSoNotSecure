7676559dc5ec86a0838990b8dff5bec5abec3d25
JCT-Biura <88521658+JCT-B@users.noreply.github.com>
2025-10-18 10:43:44 +0545

    changed the width of window main from 400 to 380

--- main.c
+++ main.c
@@ -105,7 +105,7 @@ void mainProgram() {
 
     //Init of windowMain
     windowMain = gtk_window_new();
-    gtk_window_set_default_size(GTK_WINDOW(windowMain),400,400);
+    gtk_window_set_default_size(GTK_WINDOW(windowMain),380,400);
     gtk_window_set_title(GTK_WINDOW(windowMain),"NotSoNotSecure");
     gtk_window_present(GTK_WINDOW(windowMain));
 
